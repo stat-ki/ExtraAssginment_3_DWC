db0d5a1287ef9f5d9a8725cb1e01e09606a18c80
stat-ki <per_ik@outlook.com>
2020-03-05 14:41:27 +0000

    #14 Create BookComments model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_065709) do
+ActiveRecord::Schema.define(version: 2020_03_05_142936) do
+
+  create_table "book_commnets", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
