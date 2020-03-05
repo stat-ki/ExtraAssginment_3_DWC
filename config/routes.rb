24cfb69be8198c39c69ce2c5d2eb2a379d28e1b5
stat-ki <per_ik@outlook.com>
2020-03-05 07:04:08 +0000

    #4

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :favorites, only: [:create, :destroy]
   root 'home#top'
   get 'home/about' => 'home#about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_054603) do
+ActiveRecord::Schema.define(version: 2020_03_05_065709) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
