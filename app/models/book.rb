63f94b49891d021b4824c92bcbd3851836e9a30f
stat-ki <59187251+stat-ki@users.noreply.github.com>
2020-03-06 00:16:59 +0900

    Revert "#14 Create BookComments model & controller"

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
 	belongs_to :user
 	has_many :favorites
-	has_many :book_comments
 	#バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
 	#presence trueは空欄の場合を意味する。
 	validates :title, presence: true
