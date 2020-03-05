ccbca645191ede281c98213f485f72bc7dc2c15c
stat-ki <per_ik@outlook.com>
2020-03-05 07:37:50 +0000

    #6 Modify favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,21 @@
 class FavoritesController < ApplicationController
 
   def create
+    favorite = Favorite.new(
+      user_id = current_user.user_id,
+      book_id = params[:book_id]
+    )
+    favorite.save
+    redirect_to(:back)
   end
 
   def destroy
+    favorite = Favorite.find_by(
+      user_id = current_user.user_id,
+      book_id = params[:book_id]
+    )
+    favorite.destroy
+    redirect_to(:back)
   end
 
 end
